e9fcefd470d2fa0da4d6026478159eb4379977b5
abhishektanwar2309 <abhishek16006@iiitd.ac.in>
2025-04-06 09:14:13 +0530

    v5: Interpolation

--- blog-angular/src/app/app.component.ts
+++ blog-angular/src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog-angular';
+  appName = 'Blog App';
+  name='Abhishek Tanwar';
+  getName(){
+    return this.name;
+  };
+  obj={
+    name:'peter',
+    age:30
+  };
+  arr=['one','two','three'];
+  a=100;
+  b=200;
+  siteUrl=window.location.href;
+
 }
